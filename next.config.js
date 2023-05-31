2912430f033af5b01bc76bb36edcf21ebce72d68
Eduardo Solanas <eduardo.solanas@gmail.com>
2023-05-31 19:05:12 +0100

    using export static by default

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@
 const path = require('path');
 
 module.exports = {
+    images: {
+        unoptimized: true,
+    },
+    output: 'export',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
     },
